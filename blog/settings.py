b3f384d69e68b008a57faef9c2d7c27267301896
Manju Muthukumaran <meetmanjusri@gmail.com>
2021-11-28 00:12:31 -0600

    Updated registration templates

--- blog/settings.py
+++ blog/settings.py
@@ -127,6 +127,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'smtpdjango2021@gmail.com'
+EMAIL_HOST_PASSWORD = 'Assignment1!'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 
